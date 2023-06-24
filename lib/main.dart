8152d4722767c65a09b93498a0b84be2d9322e9f
Draaft4 <draaft@outlook.com>
2023-06-24 11:42:16 -0500

    agregar metodos cloud messaging

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,36 @@ import 'package:app_fidelizacion/app/routes/pages.dart';
 import 'package:app_fidelizacion/app/ui/pages/splash_page/splash_page.dart';
 import 'package:app_fidelizacion/app/ui/theme/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  //final fcmToken = await FirebaseMessaging.instance.getToken();
+  //token = 
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print("${settings.authorizationStatus}");
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) { 
+    print(message.data);
+    if(message.notification!=null){
+      print('${message.notification}');
+    }
+  });
+  final fcmToken = await messaging.getToken();
+  print(fcmToken);
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.INITIAL,
